dc4c93d910ac61028e278ae068c9cfb18572e014
Simon Smith <71359986+ssmith828@users.noreply.github.com>
2025-03-26 16:59:30 -0400

    Add files via upload

--- agent-app/scripts/config.js
+++ agent-app/scripts/config.js
@@ -1,13 +1,13 @@
 export default {
     // 'development' or 'production'
-    environment: 'development',
+    environment: 'production',
 
     // Using local test servers
-    developmentUri: 'http://localhost:8080',
+    developmentUri: 'https://joshestrada.github.io/pexip-genesys-agent/agent-app',
 
     // Publicly accessible location where the admin-app files are hosted.
     // This is different than the Pexip conference node value below.
-    prodUri:  'https://pexip-agent-app.example.com/path/to/app',
+    prodUri:  'https://joshestrada.github.io/pexip-genesys-agent/agent-app',
 
     // Id for the video DOM element. Only change this if you customize index.html.
     videoElementId: "pexip-video-container",
@@ -16,21 +16,21 @@ export default {
         // Genesys Cloud region
         // 'mypurecloud.com', 'usw2.pure.cloud', 'mypurecloud.ie', 'euw2.pure.cloud', 'mypurecloud.com.au'
         // See https://help.mypurecloud.com/articles/aws-regions-for-genesys-cloud-deployment/ for all options
-        region: 'mypurecloud.com',
+        region: 'usw2.pure.cloud',
 
         // OAuth Client ID
         // Created in "Create a Token Implicit OAuth Grant for Genesys Cloud deployment" step
-        oauthClientID: '1111111a-1bc1-11bb-a1bb-1b1bb111ca11'
+        oauthClientID: '9708dcf4-5e6b-4b2e-9639-0b91c63b8089'
     },
 
     pexip: {
         // Used to identify the conference attendee for proper handling by Pexip Infinity local policy.
-        conferencePrefix: "xyz",
+        conferencePrefix: "app_",
 
         // External domain for Pexip Infinity Edge/Transcoding nodes.
-        conferenceNode: "pexip.example.com",
+        conferenceNode: "pxpconfdfwprd01.connect.rbfcu.org",
 
         // Conference PIN. Must match the PIN number set by Pexip Infinity local policy for ad-hoc conference creation.
-        conferencePin: "1234"
+        conferencePin: "2021"
     }
 }
